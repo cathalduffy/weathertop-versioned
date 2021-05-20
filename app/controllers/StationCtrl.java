06a41f80eb10de72b38bb7ce932cd6abb3e569c5
Cathal Duffy <cathalduffy89@gmail.com>
2021-05-20 16:29:43 +0100

    Icons added

--- app/controllers/StationCtrl.java
+++ app/controllers/StationCtrl.java
@@ -14,12 +14,21 @@ public class StationCtrl extends Controller
 
     public static void index(Long id)
     {
- //       Station station = Station.findById(id);
-        //        Logger.info ("Station id = " + id);
-        //        render("station.html", station);
+       Station station = Station.findById(id);      //getting the readings by the station id and displaying in stations page
+         {
+            if (station.readings.size() > 0) {
+                station.setWeatherConditions(codeToString(station.readings.get(station.readings.size() - 1).code));
 
+                station.setWindToBeau(windToBeaufort(station.readings.get(station.readings.size() - 1).windSpeed));
 
+                station.setWindCompass(windDirectionCompass(station.readings.get(station.readings.size() - 1).windDirection));
 
+                station.setWeatherIcon(weatherIcon(station.readings.get(station.readings.size() - 1).windDirection));
+
+            }
+        }
+               Logger.info ("Station id = " + id);
+                render("station.html", station);
     }
 
 
